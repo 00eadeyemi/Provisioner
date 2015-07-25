a0459f3317e106f373541f01be411e3af916085f
Sean Leach <seanleach@Seans-MacBook-Air.local>
2015-07-25 18:32:48 +0100

    Provisioning script for new mac

---
+++ provision.sh
@@ -0,0 +1,13 @@
+#defaults write com.apple.finder CreateDesktop -bool false && killall Finder
+#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo agree >> sudo xcodebuild -license
+brew install wget
+brew install ruby
+brew install node
+brew install caskroom/cask/brew-cask
+brew cask install google-chrome
+brew cask install charles
+brew cask install virtualbox
+brew cask install vagrant
+
+
